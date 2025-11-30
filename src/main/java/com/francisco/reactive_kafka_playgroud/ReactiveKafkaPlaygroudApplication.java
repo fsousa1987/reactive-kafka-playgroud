997b11138808bb3fb9f948eb774887bf9a3eb508
Francisco Sousa <franciscoeds1987@gmail.com>
2025-11-30 10:32:59 -0300

    Project Setup

---
+++ src/main/java/com/francisco/reactive_kafka_playgroud/ReactiveKafkaPlaygroudApplication.java
@@ -0,0 +1,13 @@
+package com.francisco.reactive_kafka_playgroud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveKafkaPlaygroudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveKafkaPlaygroudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/francisco/reactive_kafka_playgroud/ReactiveKafkaPlaygroudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francisco.reactive_kafka_playgroud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveKafkaPlaygroudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
