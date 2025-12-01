9e969c0b6fd217e1fda6fc1b9e7d0a6076033a8f
Francisco Sousa <franciscoeds1987@gmail.com>
2025-12-01 07:23:44 -0300

    Creating Kafka Receiver

--- src/main/java/com/francisco/reactive_kafka_playgroud/sec01/Lec01KafkaConsumer.java
+++ src/main/java/com/francisco/reactive_kafka_playgroud/sec01/Lec01KafkaConsumer.java
@@ -2,7 +2,12 @@ package com.francisco.reactive_kafka_playgroud.sec01;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
 
+import java.util.List;
 import java.util.Map;
 
 /*
@@ -12,18 +17,26 @@ import java.util.Map;
 */
 public class Lec01KafkaConsumer {
 
+    private static final Logger LOG = LoggerFactory.getLogger(Lec01KafkaConsumer.class);
+
     static void main() {
 
-        var consumerConfig = Map.of(
+        var consumerConfig = Map.<String, Object>of(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.GROUP_ID_CONFIG, "demo-group"
         );
 
-        // ReceiverOptions.create();
-        // KafkaReceiver.create();
+        var options = ReceiverOptions
+                .create(consumerConfig)
+                .subscription(List.of("order-events"));
 
+        KafkaReceiver
+                .create(options)
+                .receive()
+                .doOnNext(r -> LOG.info("key: {}, value: {}", r.key(), r.value()))
+                .subscribe();
     }
 
 }
