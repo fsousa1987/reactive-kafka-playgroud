21fccca56cb03aaa41e534d930e8a3c138cf323d
Francisco Sousa <franciscoeds1987@gmail.com>
2025-12-05 11:21:59 -0300

    Emitting 1 Million Events

---
+++ src/main/java/com/francisco/reactive_kafka_playgroud/sec03/KafkaProducer.java
@@ -0,0 +1,51 @@
+package com.francisco.reactive_kafka_playgroud.sec03;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.kafka.sender.KafkaSender;
+import reactor.kafka.sender.SenderOptions;
+import reactor.kafka.sender.SenderRecord;
+
+import java.util.Map;
+
+/*
+    goal: to demo a simple Kafka producer using reactor-kafka
+*/
+public class KafkaProducer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaProducer.class);
+
+    static void main() {
+
+        var producerConfig = Map.<String, Object>of(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class
+        );
+
+        var options = SenderOptions
+                .<String, String>create(producerConfig)
+                .maxInFlight(10_000);
+
+        var flux = Flux.range(1, 1_000_000)
+                .map(i -> new ProducerRecord<>("order-events", i.toString(), "order-" + i))
+                .map(pr -> SenderRecord.create(pr, pr.key()));
+
+        var start = System.currentTimeMillis();
+
+        var sender = KafkaSender.create(options);
+
+        sender.send(flux)
+                .doOnNext(r -> LOG.info("correlation id: {}", r.correlationMetadata()))
+                .doOnComplete(() -> {
+                    LOG.info("Total time taken: {} ms", (System.currentTimeMillis() - start));
+                    sender.close();
+                })
+                .subscribe();
+    }
+
+}
