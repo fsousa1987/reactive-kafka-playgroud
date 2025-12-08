725554a00683c67f9f6a821094cb39385817b407
Francisco Sousa <franciscoeds1987@gmail.com>
2025-12-08 09:10:07 -0300

    Seeking Offset

---
+++ src/main/java/com/francisco/reactive_kafka_playgroud/sec07/KafkaConsumer.java
@@ -0,0 +1,51 @@
+package com.francisco.reactive_kafka_playgroud.sec07;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+    goal: to seek offset
+*/
+public class KafkaConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    static void main() {
+
+        var consumerConfig = Map.<String, Object>of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, "demo-group-123",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
+        );
+
+        var options = ReceiverOptions
+                .create(consumerConfig)
+                .addAssignListener(c -> {
+                    c.forEach(r -> LOG.info("assigned {}", r.position()));
+                    // c.forEach(r -> r.seek(r.position() - 2));
+                    c.stream()
+                            .filter(r -> r.topicPartition().partition() == 2)
+                            .findFirst()
+                            .ifPresent(r -> r.seek(r.position() - 2)); // seek value can not be -ve. ensure before setting
+                })
+                .subscription(List.of("order-events"));
+
+        KafkaReceiver
+                .create(options)
+                .receive()
+                .doOnNext(r -> LOG.info("key: {}, value: {}", r.key(), r.value()))
+                .doOnNext(r -> r.receiverOffset().acknowledge())
+                .subscribe();
+    }
+
+}
