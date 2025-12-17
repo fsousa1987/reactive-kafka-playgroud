89e4740b2b61cb002692d811487a1479de25d1ea
Francisco Sousa <franciscoeds1987@gmail.com>
2025-12-17 11:53:27 -0300

    Batch - Receiver Auto Ack / concatMap

--- src/main/java/com/francisco/reactive_kafka_playgroud/sec09/KafkaConsumer.java
+++ src/main/java/com/francisco/reactive_kafka_playgroud/sec09/KafkaConsumer.java
@@ -1,12 +1,16 @@
 package com.francisco.reactive_kafka_playgroud.sec09;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 
+import java.time.Duration;
 import java.util.List;
 import java.util.Map;
 
@@ -25,19 +29,29 @@ public class KafkaConsumer {
                 ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
                 ConsumerConfig.GROUP_ID_CONFIG, "demo-group",
                 ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
-                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
+                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1",
+                ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 3
         );
 
         var options = ReceiverOptions
                 .create(consumerConfig)
+                .commitInterval(Duration.ofSeconds(1))
                 .subscription(List.of("order-events"));
 
         KafkaReceiver
                 .create(options)
-                .receive()
-                .doOnNext(r -> LOG.info("key: {}, value: {}", r.key(), r.value()))
-                .doOnNext(r -> r.receiverOffset().acknowledge())
+                .receiveAutoAck()
+                .log()
+                .concatMap(KafkaConsumer::batchProcess)
                 .subscribe();
+    }
+
+    private static Mono<Void> batchProcess(Flux<ConsumerRecord<Object, Object>> flux) {
+        return flux
+                .doFirst(() -> LOG.info("----------------------"))
+                .doOnNext(r -> LOG.info("key: {}, value: {}", r.key(), r.value()))
+                .then(Mono.delay(Duration.ofSeconds(1)))
+                .then();
     }
 
 }
