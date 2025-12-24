48fd4d33356967233ba40e42b4a63ae8047d115c
Francisco Sousa <franciscoeds1987@gmail.com>
2025-12-24 17:45:45 -0300

    Poison Pill Messages

---
+++ src/main/java/com/francisco/reactive_kafka_playgroud/sec14/KafkaConsumer.java
@@ -0,0 +1,43 @@
+package com.francisco.reactive_kafka_playgroud.sec14;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.kafka.receiver.KafkaReceiver;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.List;
+import java.util.Map;
+
+/*
+    goal: to demo poison pill messages
+*/
+public class KafkaConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(KafkaConsumer.class);
+
+    static void main() {
+
+        var consumerConfig = Map.<String, Object>of(
+                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class,
+                ConsumerConfig.GROUP_ID_CONFIG, "demo-group",
+                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
+        );
+
+        var options = ReceiverOptions.<String, Integer>create(consumerConfig)
+                .subscription(List.of("order-events"));
+
+        KafkaReceiver
+                .create(options)
+                .receive()
+                .doOnNext(r -> LOG.info("key: {}, value: {}", r.key(), r.value()))
+                .doOnNext(r -> r.receiverOffset().acknowledge())
+                .subscribe();
+    }
+
+}
