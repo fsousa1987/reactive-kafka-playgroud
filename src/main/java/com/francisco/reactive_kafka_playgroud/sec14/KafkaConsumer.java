241bb11c22d731648614b75d226b3c2da33bf5e2
Francisco Sousa <franciscoeds1987@gmail.com>
2025-12-26 10:00:38 -0300

    Poison Pill Messages - Fix

--- src/main/java/com/francisco/reactive_kafka_playgroud/sec14/KafkaConsumer.java
+++ src/main/java/com/francisco/reactive_kafka_playgroud/sec14/KafkaConsumer.java
@@ -5,6 +5,7 @@ import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import reactor.kafka.receiver.KafkaReceiver;
 import reactor.kafka.receiver.ReceiverOptions;
 
@@ -23,13 +24,14 @@ public class KafkaConsumer {
         var consumerConfig = Map.<String, Object>of(
                 ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
                 ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class,
+                // ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class,
                 ConsumerConfig.GROUP_ID_CONFIG, "demo-group",
                 ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
                 ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
         );
 
         var options = ReceiverOptions.<String, Integer>create(consumerConfig)
+                .withValueDeserializer(errorHandlingDeserializer())
                 .subscription(List.of("order-events"));
 
         KafkaReceiver
@@ -38,6 +40,17 @@ public class KafkaConsumer {
                 .doOnNext(r -> LOG.info("key: {}, value: {}", r.key(), r.value()))
                 .doOnNext(r -> r.receiverOffset().acknowledge())
                 .subscribe();
+    }
+
+    private static ErrorHandlingDeserializer<Integer> errorHandlingDeserializer() {
+        var deserializer = new ErrorHandlingDeserializer<>(new IntegerDeserializer());
+        deserializer.setFailedDeserializationFunction(
+                info -> {
+                    LOG.error("failed record: {}", new String(info.getData()));
+                    return -10_000;
+                }
+        );
+        return deserializer;
     }
 
 }
